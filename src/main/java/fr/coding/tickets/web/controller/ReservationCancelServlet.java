4fd357776cb1c80f45e09b094f9184e046a5a022
Papa Abdou Mbodji <abdoumbodji1401@gmail.com>
2025-12-09 13:50:54 +0100

    Docker with maven

--- src/main/java/fr/coding/tickets/web/controller/ReservationCancelServlet.java
+++ src/main/java/fr/coding/tickets/web/controller/ReservationCancelServlet.java
@@ -7,12 +7,12 @@ import fr.coding.tickets.model.Client;
 import fr.coding.tickets.model.ReservationInvalideException;
 import fr.coding.tickets.model.Utilisateur;
 import fr.coding.tickets.service.TicketService;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/reservations/cancel")
 public class ReservationCancelServlet extends HttpServlet {
